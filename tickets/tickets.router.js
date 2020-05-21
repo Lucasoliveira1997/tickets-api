f0772d2fba6821f9a15287ccd7f09f93de35d076
Lucas Silva de Oliveira <ti.lucas@gruponortene.homeip.net>
2020-05-21 16:31:03 -0300

    it was created post and get endpoints on tickets router

---
+++ tickets/tickets.router.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const errors = require('restify-errors')
+const Ticket = require('./tickets.model')
+
+module.exports = server => {
+    server.post('/tickets', async(req, resp, next) => {
+        if(!req.is('application/json')) {
+            return next(new errors.InvalidContentError("Expects 'application/json"))
+        }
+
+        const { user, about, description } = req.body
+
+        try {
+            const ticket = await new Ticket({user, about, description})
+            const newTicket = ticket.save()
+            resp.status(201)
+            resp.send(newTicket)
+            return next()
+        } catch (error) {
+            return next(new errors.InternalError(error.message))
+        }
+    })
+
+    server.get('/tickets', async(req, resp, next) => {
+        try {
+            const user = await Ticket.find({})
+            resp.status(200)
+            resp.send(user)
+            return next()
+
+        } catch (error) {
+            return next(new errors.InvalidContentError(error.message))
+        }
+    })
+    server.get('/tickets/:id', async(req, resp, next) => {
+
+    })
+    server.put('/tickets/:id', async(req, resp, next) => {
+
+    })
+    server.del('/tickets/:id', async(req, resp, next) => {
+
+    })
+}
