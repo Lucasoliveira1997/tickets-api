b49110906dd5cc4d07553f2ebc41a69f5318649c
Lucasoliveira1997 <lsdoliveira97@gmail.com>
2020-06-06 00:03:18 -0300

    updated users post,getById,put and delete  methods making generic functions (save, get, getById, update, delete) to be used for more endpoints

--- common/model.router.js
+++ common/model.router.js
@@ -9,16 +9,60 @@ class ModelRouter extends Router {
         super(Router)
         this._model = mongoose.model(model)
 
-        this.get = async(req, resp, next) => {
+        this.get = async (req, resp, next) => {
             try {
-                const document = await this._model.find({})   
-                return this.render(resp, next, document)                    
+                const document = await this._model.find({})
+                return this.render(resp, next, document)
             } catch (error) {
                 next(new errors.NotFoundError('Document Not Found'))
             }
         }
+
+        this.getById = async (req, resp, next) => {
+            try {
+                const document = await this._model.findById(req.params.id)
+                await this.render(resp, next, document)
+            } catch (error) {
+                return next(new errors.InvalidContentError(error.message))
+            }
+        }
+
+        this.save = async (req, resp, next) => {
+            if (!req.is('application/json')) {
+                return next(new errors.InvalidContentError("Expects 'application/json'"))
+            }
+
+            try {
+                const model = await new this._model(req.body)
+                const document = await model.save()
+                await router.render(resp, next, document)
+            } catch (error) {
+                return next(new errors.InternalError(error.message))
+            }
+        }
+
+        this.update = async (req, resp, next) => {
+            try {
+                const options = { new: true }
+                const document = await User.findByIdAndUpdate(req.params.id, req.body, options)
+                await router.render(resp, next, document)
+                return next()
+            } catch (error) {
+                return next(new errors.InvalidContentError(error.message))
+            }
+        }
+
+        this.remove = async (req, resp, next) => {
+            try {
+                await this._model.findByIdAndRemove(req.params.id)
+                resp.send(204)
+                return next()
+            } catch (error) {
+                return next(new errors.InvalidContentError(error.message))
+            }
+        }
     }
-    
+
 }
 
 module.exports = ModelRouter
--- users/users.router.js
+++ users/users.router.js
@@ -5,51 +5,13 @@ const _modelRouter = require('../common/model.router')
 const modelRouter = new _modelRouter('User')
 
 module.exports = server => {
-    server.post('/users', async (req, resp, next) => {
-        if(!req.is('application/json')){
-            return next(new errors.InvalidContentError("Expects 'application/json'"))
-        }
-
-        const {name, password, email, department, phone, status, category} = req.body
-
-        try {
-            const user = await new User({ name, email, department, password, phone, status, category })
-            const newUser = await user.save()
-            await router.render(resp, next, newUser)
-        } catch (error) {
-            return next(new errors.InternalError(error.message))
-        }
-    })
+    server.post('/users', modelRouter.save)
 
     server.get('/users', modelRouter.get)
 
-    server.get('/users/:id', async (req, resp, next) => {
-        try {
-            const user = await User.findById(req.params.id)                
-            await router.render(resp, next, user)
-        } catch (error) {
-            return next(new errors.InvalidContentError(error.message))
-        }
-    })
+    server.get('/users/:id', modelRouter.getById)
 
-    server.put('/users/:id', async (req, resp, next) => {
-        try {
-            const options = {new: true}
-            const user = await User.findByIdAndUpdate(req.params.id, req.body, options)
-            await router.render(resp, next, user)
-            return next()
-        } catch (error) {
-            return next(new errors.InvalidContentError(error.message))
-        }
-    })
+    server.put('/users/:id', modelRouter.update)
 
-    server.del('/users/:id', async (req, resp, next) => {
-        try {
-            await User.findByIdAndRemove(req.params.id)
-            resp.send(204)
-            return next()
-        } catch (error) {
-            return next(new errors.InvalidContentError(error.message))
-        }
-    })
+    server.del('/users/:id', modelRouter.remove)
 }
